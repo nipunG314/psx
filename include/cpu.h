2b7e610b2cee9eb780b4e1572124df23387343a3
Nipun Garg <nipung271@gmail.com>
2021-11-10 16:56:26 +0530

    CPU: Add interconnect to cpu

--- include/cpu.h
+++ include/cpu.h
@@ -1,12 +1,15 @@
 #include <stdint.h>
 
+#include "interconnect.h"
+
 #ifndef CPU_H
 #define CPU_H
 
 typedef struct Cpu {
   uint32_t pc;
+  Interconnect inter;
 } Cpu;
 
-Cpu init_cpu();
+Cpu init_cpu(char const *bios_filename);
 
 #endif
--- src/cpu.c
+++ src/cpu.c
@@ -1,10 +1,11 @@
 #include "cpu.h"
 #include "interconnect.h"
 
-Cpu init_cpu() {
+Cpu init_cpu(char const *bios_filename) {
   Cpu cpu = {0};
   
   cpu.pc = BIOS_START;
+  cpu.inter = init_interconnect(bios_filename);
 
   return cpu;
 }
