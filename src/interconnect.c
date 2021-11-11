794083542be7ab515b4e4ede7f8e4a44f2aa89b7
Nipun Garg <nipung271@gmail.com>
2021-11-11 14:18:39 +0530

    INTER: Add unaligned memory checks

--- src/interconnect.c
+++ src/interconnect.c
@@ -14,6 +14,11 @@ Interconnect init_interconnect(char const *bios_filename) {
 }
 
 uint32_t load_inter32(Interconnect *inter, uint32_t addr) {
+  if (addr % 4) {
+    log_error("Unaligned load_inter32 addr: 0x%X", addr);
+    exit(EXIT_FAILURE);
+  }
+
   int32_t offset = range_contains(BIOS_START, BIOS_SIZE, addr);
   if (offset >= 0) {
     return load_bios32(&inter->bios, offset);
@@ -24,6 +29,11 @@ uint32_t load_inter32(Interconnect *inter, uint32_t addr) {
 }
 
 void store_inter32(Interconnect *inter, uint32_t addr, uint32_t val) {
+  if (addr % 4) {
+    log_error("Unaligned store_inter32 addr: 0x%X", addr);
+    exit(EXIT_FAILURE);
+  }
+
   log_error("STUB: store_inter32: addr: 0x%X, val: 0x%X", addr, val);
   exit(EXIT_FAILURE);
 }
