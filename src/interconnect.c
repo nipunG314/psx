915eb4c09a117140933b6331dca0b057b3f4e0c1
Nipun Garg <nipung271@gmail.com>
2021-11-12 00:43:26 +0530

    INTER: Implement partial store_inter32 call

--- src/interconnect.c
+++ src/interconnect.c
@@ -28,5 +28,21 @@ void store_inter32(Interconnect *inter, uint32_t addr, uint32_t val) {
   if (addr % 4)
     error("Unaligned store_inter32 addr: 0x%X", addr);
 
-  error("STUB: store_inter32: addr: 0x%X, val: 0x%X", addr, val);
+  int32_t offset = range_contains(MEM_CONTROL_START, MEM_CONTROL_SIZE, addr);
+  if (offset >= 0) {
+    switch (offset) {
+      case 0:
+        if (val != 0x1F000000)
+          error("Bad Expansion 1: Base Address: 0x%X", val);
+      case 4:
+        if (val != 0x1F802000)
+          error("Bad Expansion 2: Base Address: 0x%X", val);
+      default:
+        log_error("Unhandled Write to MEM_CONTROL register");
+        return;
+
+    }
+  }
+
+  error("Unhandled load call. addr: 0x%X, val: 0x%X", addr, val);
 }
