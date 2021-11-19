e4db13b9a18315eb71569fc65a79eed994e2ae47
Nipun Garg <nipung271@gmail.com>
2021-11-19 15:23:29 +0530

    RAM: Fix store_ram32

--- src/ram.c
+++ src/ram.c
@@ -25,9 +25,9 @@ uint32_t load_ram32(Ram *ram, Addr offset) {
 void store_ram32(Ram *ram, Addr offset, uint32_t val) {
 
   uint8_t b0 = val;
-  uint8_t b1 = val << 8;
-  uint8_t b2 = val << 16;
-  uint8_t b3 = val << 24;
+  uint8_t b1 = val >> 8;
+  uint8_t b2 = val >> 16;
+  uint8_t b3 = val >> 24;
 
   ram->data[offset.data++] = b0; 
   ram->data[offset.data++] = b1; 
