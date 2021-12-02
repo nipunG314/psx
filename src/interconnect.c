b826f6296d91a36e4f59d118afe8409131d3f156
Nipun Garg <nipung271@gmail.com>
2021-12-02 16:28:47 +0530

    INTER: Fix increment step for clearing table

--- src/interconnect.c
+++ src/interconnect.c
@@ -341,8 +341,7 @@ void perform_dma_block(Interconnect *inter, DmaPort port) {
   START_LOGGING_PC();
 
   DmaChannel *channel = inter->dma.channels + port;
-  int8_t _increment = 4 - 8 * channel->step;
-  uint8_t increment = _increment;
+  int8_t increment = 4 - 8 * channel->step;
   Addr addr = channel->base_address;
   uint32_t transfer_size = get_dma_channel_transfer_size(channel);
 
